e35abd4caedad1d5a917f8191c208e3a80b2c12b
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-18 23:23:15 -0700

    install express-session and configured the sessionConfig

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const server = express();
+const session = require("express-session");
 
 // routers
 const usersRouter = require("../users/users-router");
@@ -10,6 +11,20 @@ const authRouter = require("../auth/auth-router");
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
+const sessionConfig = {
+  name: "sessionId",
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, // https
+    httpOnly: true // when true, js can't get to the cookie
+  },
+  // we should only save sessions when user allows it
+  resave: false,
+  saveUninitialized: false
+};
+server.use(session(sessionConfig));
 
 server.use("/api/users", usersRouter);
 server.use("/api/auth", authRouter);
