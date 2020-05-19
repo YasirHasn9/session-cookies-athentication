0bb1514679dc6bed4113fe27b572ee2b72dbc233
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-18 21:42:10 -0700

    create users table

--- database/migrations/20200518212740_init.js
+++ database/migrations/20200518212740_init.js
@@ -2,13 +2,29 @@ exports.up = async function(knex) {
   await knex.schema.createTable("roles", role => {
     role.increments("id");
     role
-      .string("username", 128)
+      .string("name", 128)
       .notNullable()
       .unique();
-    role.boolean("role").defaultTo(false);
+  });
+
+  await knex.schema.createTable("users", user => {
+    user.increments("id");
+    user
+      .string("username", 128)
+      .notNullable()
+      .unique()
+      .index();
+    user.string("password", 128).notNullable();
+    user
+      .int("role")
+      .unsigned()
+      .references("role.id")
+      .onDelete("RESTRICT")
+      .onUpdate("CASCADE");
   });
 };
 
 exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("users");
   await knex.schema.dropTableIfExists("roles");
 };
